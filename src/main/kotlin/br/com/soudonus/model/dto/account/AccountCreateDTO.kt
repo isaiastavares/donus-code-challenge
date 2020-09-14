ad4742d045921cad859c524d1afc65cf46b1c649
isaiastavares <isaiasengsoft@gmail.com>
2020-09-13 22:22:38 -0300

    validate notblank name and cpf

--- src/main/kotlin/br/com/soudonus/model/dto/account/AccountCreateDTO.kt
+++ src/main/kotlin/br/com/soudonus/model/dto/account/AccountCreateDTO.kt
@@ -1,11 +1,13 @@
 package br.com.soudonus.model.dto.account
 
+import org.apache.commons.lang3.StringUtils
 import org.hibernate.validator.constraints.br.CPF
 import javax.validation.constraints.NotBlank
 
 data class AccountCreateDTO(
         @field:NotBlank
-        val name: String,
+        val name: String = StringUtils.EMPTY,
+        @field:NotBlank
         @field:CPF
-        val cpf: String
+        val cpf: String = StringUtils.EMPTY
 )
